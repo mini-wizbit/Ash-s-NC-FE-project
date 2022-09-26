a659172ae15720527f0e1a74f7307887d81e6e69
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-26 16:54:58 +0100

    woop we can see just this review id

--- nc-games/src/components/ReviewById/ReviewById.jsx
+++ nc-games/src/components/ReviewById/ReviewById.jsx
@@ -1,4 +1,35 @@
+import React from "react"
+import { useEffect } from "react"
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+import { getReviewById } from "../../utils/api"
+
+
 
 export const ReviewById = () =>{
-    //code
+    const { review_id } = useParams()
+    const [reviewNumber, setReviewNumber]= useState(1)
+
+
+    useEffect(()=>{
+        getReviewById(review_id)
+        .then((thisReview)=>{
+            setReviewNumber(thisReview)
+        })
+    },[review_id]) 
+    
+
+    return (
+        <div className="reviewById-card">
+            <li>
+                <h2>{reviewNumber.title}</h2>
+                <img src={reviewNumber.review_img_url} alt={reviewNumber.title} className="reviewById-card-Img"></img>
+                <p>{reviewNumber.review_body}</p>
+                <h4>{reviewNumber.votes}</h4>
+                <h4>{reviewNumber.owner}</h4>
+                <p>{reviewNumber.category}</p>
+            </li>
+
+        </div>
+    )
 }
