a913e7afa52601f4468cda1afe1bd06b962cf2c9
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-26 16:53:40 +0100

    pathing for review by ID

--- nc-games/src/utils/api.js
+++ nc-games/src/utils/api.js
@@ -7,9 +7,7 @@ const gamesApi = axios.create({
 export const getReviews = (category) => {
   let queryStr = "/reviews";
   if (category) queryStr += `?category=${category}`;
-  console.log(queryStr, "< is this right?");
   return gamesApi.get(queryStr).then(({ data }) => {
-    console.log(data, "from api");
     return data;
   });
 };
