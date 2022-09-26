6a2b49888f9dea94b06cdab4ff8961b4e6201bf4
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-26 14:20:16 +0100

    all reviews no styling

--- nc-games/src/components/AllReviews/AllReviews.jsx
+++ nc-games/src/components/AllReviews/AllReviews.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getReviews } from "../../utils/api";
+
 
 export const AllReviews = ()=>{
-    // use util from this morning 
 
-    //then use effect
+    const [reviews, setReviews] = useState([])
+    useEffect(()=>{
+        getReviews().then((ApiReviews)=>{
+            console.log(ApiReviews);
+            setReviews(ApiReviews.reviewArray)
+        })
+    }, [])
 
-    // set state
+    return (
+        <div>
+            <p>placeholder for set category</p>
+            <p>sort by X</p>
+            <ul>
+                <div className="review-list">
+                    {reviews.map((review) =>{
+                        return(
+                            <div>
+                            <li>
+                                <div className="review-card">
+                                    <h2 className="review-title">{review.title}</h2>
+                                    <img src={review.review_img_url} alt={review.title}></img>
+                                    <h3 className="review-owner">Review by {review.owner}</h3>
+                                    <h4 className="review-votes"> Votes:{review.votes}</h4>
+                                    <p className="review-category">Category:{review.category}</p>
+                                    <p key={review.review_id}>placeholder for a link?</p>
+                                </div>
+                            </li>
+                            </div>
+                        )
+                    })}
+                </div>
+            </ul>
+        </div>
+    )
 
-    // return map of state
 }
