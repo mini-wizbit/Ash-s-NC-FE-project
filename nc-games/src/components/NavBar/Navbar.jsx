0558fa31529babe69ca056c8c11735d13b967016
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-26 14:05:47 +0100

    missed the return

--- nc-games/src/components/NavBar/Navbar.jsx
+++ nc-games/src/components/NavBar/Navbar.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar =()=>{
-    <div>
+    return(
+
+        <div>
         <nav>
             <Link to="/">Home</Link>
             <Link to="/api/reviews">Game Reviews</Link>
         </nav>
     </div>
+        )
 }
