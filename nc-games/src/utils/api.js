306a8750f803d7292a2f0a43e592700e66f80531
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-26 15:48:43 +0100

    need to fix BE-api

--- nc-games/src/utils/api.js
+++ nc-games/src/utils/api.js
@@ -4,8 +4,24 @@ const gamesApi = axios.create({
   baseURL: "https://ashs-nc-games-project.herokuapp.com/api",
 });
 
-export const getReviews = () => {
-  return gamesApi.get(`/reviews`).then(({ data }) => {
+export const getReviews = (category) => {
+  let queryStr = "/reviews";
+  if (category) queryStr += `?category=${category}`;
+  console.log(queryStr, "< is this right?");
+  return gamesApi.get(queryStr).then(({ data }) => {
+    console.log(data, "from api");
     return data;
+  });
+};
+
+export const getCategories = () => {
+  return gamesApi.get("/categories").then(({ data }) => {
+    return data.categories;
+  });
+};
+
+export const getReviewsByCategories = (category) => {
+  return gamesApi.get(`/reviews?category=${category}`).then(({ data }) => {
+    return data.reviews;
   });
 };
