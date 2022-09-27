f2d32bd429544cfdbb485eff1f90da30185450cc
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-27 11:48:10 +0100

    fix merge error of no button

--- nc-games/src/components/ReviewById/ReviewById.jsx
+++ nc-games/src/components/ReviewById/ReviewById.jsx
@@ -10,15 +10,24 @@ export const ReviewById = () => {
   const { review_id } = useParams();
   const [reviewById, setReviewById] = useState(1);
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true);
-    getReviewById(review_id).then((thisReview) => {
-      setLoading(false);
-      setReviewById(thisReview);
-    });
-  }, [review_id]);
-
+  const [vote, setVote]= useState(0)
+  
+  const likeVote =()=>{
+      setVote((currCount) => currCount + 1)
+      voteUpdate(review_id, 1)
+    }
+    const dislikeVote =()=>{
+        setVote((currCount) => currCount - 1)
+        voteUpdate(review_id, -1)
+    }
+    
+      useEffect(() => {
+        setLoading(true);
+        getReviewById(review_id).then((thisReview) => {
+          setLoading(false);
+          setReviewById(thisReview);
+        });
+      }, [review_id]);
 
   return loading ? (
     <p>...Loading</p>
@@ -31,7 +40,9 @@ export const ReviewById = () => {
         className="reviewById-card-Img"
       ></img>
       <p>{reviewById.review_body}</p>
-      <h4>{reviewById.votes}</h4>
+      <h4>{reviewById.votes + vote}</h4>
+      <button onClick={likeVote}>like</button>
+      <button onClick={dislikeVote}>dislike</button>
       <h4>{reviewById.owner}</h4>
       <p>{reviewById.category}</p>
     </div>
