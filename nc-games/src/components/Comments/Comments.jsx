469748165e1f7cd23fd98278d697f381b227571f
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-27 13:49:11 +0100

    fixed time issue

--- nc-games/src/components/Comments/Comments.jsx
+++ nc-games/src/components/Comments/Comments.jsx
@@ -7,15 +7,25 @@ import { commentsById } from "../../utils/api";
 export const Comments = () =>{
     const { review_id }= useParams()
     const [comments, setComments] = useState([])
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(()=>{
+        setLoading(true);
         commentsById(review_id)
         .then((apiCommentsById) => {
+            setLoading(false);
             setComments(apiCommentsById)
         })
     },[review_id])
 
-    return (
+    const realTime = (comments) =>{
+        const timeStr = comments.slice(0,10)
+        return timeStr
+    }
+    
+    
+    return loading? (<p>...loading</p>): (
         <div className="comments-box">
             <h2>Comments</h2>
             <ul>
@@ -26,7 +36,7 @@ export const Comments = () =>{
                             <h4>Made By{comment.author}</h4>
                             <p>{comment.body}</p>
                             <p>This has {comment.votes} likes</p>
-                            <p>{comment.created_at}</p>
+                            <p>{realTime(comment.created_at)}</p>
                             <p className="lineBreaker"></p>
                         </li>
                         </>
