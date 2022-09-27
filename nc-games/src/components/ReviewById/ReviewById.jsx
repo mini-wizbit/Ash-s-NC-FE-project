744acf9415a22ad44e016144f913c84d2e24c3ce
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-27 10:20:29 +0100

    added loading

--- nc-games/src/components/ReviewById/ReviewById.jsx
+++ nc-games/src/components/ReviewById/ReviewById.jsx
@@ -9,18 +9,24 @@ import { getReviewById } from "../../utils/api"
 export const ReviewById = () =>{
     const { review_id } = useParams()
     const [reviewNumber, setReviewNumber]= useState(1)
+    const [loading, setLoading] = useState(true)
 
 
     useEffect(()=>{
+        setLoading(true)
         getReviewById(review_id)
         .then((thisReview)=>{
+            setLoading(false)
             setReviewNumber(thisReview)
         })
     },[review_id]) 
     
 
-    return (
-        <div className="reviewById-card">
+    return loading ?
+            (<p>...Loading</p>):
+        (
+
+            <div className="reviewById-card">
             <li>
                 <h2>{reviewNumber.title}</h2>
                 <img src={reviewNumber.review_img_url} alt={reviewNumber.title} className="reviewById-card-Img"></img>
@@ -32,4 +38,5 @@ export const ReviewById = () =>{
 
         </div>
     )
+    
 }
