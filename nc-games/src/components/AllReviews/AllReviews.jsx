3198c267bf5ab441e06b684260beb0ff233093f8
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-27 10:50:35 +0100

    forrmated and edited

--- nc-games/src/components/AllReviews/AllReviews.jsx
+++ nc-games/src/components/AllReviews/AllReviews.jsx
@@ -5,44 +5,48 @@ import { Link } from "react-router-dom";
 import { getReviews } from "../../utils/api";
 import CategoryList from "../CategoriesList/CategoriesList.jsx";
 
+export const AllReviews = () => {
+  const [reviews, setReviews] = useState([]);
+  const [theCategory, setTheCategory] = useState("");
+  const [loading, setLoading] = useState(true);
 
-export const AllReviews = ()=>{
+  useEffect(() => {
+    setLoading(true);
+    getReviews(theCategory).then((ApiReviews) => {
+      setLoading(false);
+      setReviews(ApiReviews.reviewArray);
+    });
+  }, [theCategory]);
 
-    const [reviews, setReviews] = useState([])
-    const [theCategory, setTheCategory]=useState("")
-    const [loading, setLoading] = useState(true)
-
-    useEffect(()=>{
-        setLoading(true)
-        getReviews(theCategory).then((ApiReviews)=>{
-            setLoading(false)
-            setReviews(ApiReviews.reviewArray)
-        })
-    }, [theCategory])
-
-    return loading? (<p>...Loading please wait</p>):(
-        <div>
-            <CategoryList setTheCategory={setTheCategory}></CategoryList>
-            <p>sort by X</p>
-            <ul>
-                <div className="review-list">
-                    {reviews.map((review) =>{
-                        return(
-                            
-                            <li className="review-cards">
-                                    <h2 className="review-title">{review.title}</h2>
-                                    <img src={review.review_img_url} alt={review.title}></img>
-                                    <h3 className="review-owner">Review by {review.owner}</h3>
-                                    <h4 className="review-votes"> Votes:{review.votes}</h4>
-                                    <p className="review-category">Category:{review.category}</p>
-                                    <Link to={`/reviews/${review.review_id}`} key={review.review_id}>More Info Here</Link>
-                            </li>
-                            
-                        )
-                    })}
-                </div>
-            </ul>
+  return loading ? (
+    <p>...Loading please wait</p>
+  ) : (
+    <div>
+      <CategoryList setTheCategory={setTheCategory}></CategoryList>
+      <p>sort by X</p>
+      <ul>
+        <div className="review-list">
+          {" "}
+          {/*<- for styling in css as it will at as container */}
+          {reviews.map((review) => {
+            return (
+              <li className="review-cards">
+                <h2 className="review-title">{review.title}</h2>
+                <img src={review.review_img_url} alt={review.title}></img>
+                <h3 className="review-owner">Review by {review.owner}</h3>
+                <h4 className="review-votes"> Votes:{review.votes}</h4>
+                <p className="review-category">Category:{review.category}</p>
+                <Link
+                  to={`/reviews/${review.review_id}`}
+                  key={review.review_id}
+                >
+                  More Info Here
+                </Link>
+              </li>
+            );
+          })}
         </div>
-    )
-
-}
+      </ul>
+    </div>
+  );
+};
--- nc-games/src/components/ReviewById/ReviewById.jsx
+++ nc-games/src/components/ReviewById/ReviewById.jsx
@@ -1,42 +1,36 @@
-import React from "react"
-import { useEffect } from "react"
-import { useState } from "react"
-import { useParams } from "react-router-dom"
-import { getReviewById } from "../../utils/api"
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { getReviewById } from "../../utils/api";
 
+export const ReviewById = () => {
+  const { review_id } = useParams();
+  const [reviewById, setReviewById] = useState(1);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setLoading(true);
+    getReviewById(review_id).then((thisReview) => {
+      setLoading(false);
+      setReviewById(thisReview);
+    });
+  }, [review_id]);
 
-export const ReviewById = () =>{
-    const { review_id } = useParams()
-    const [reviewNumber, setReviewNumber]= useState(1)
-    const [loading, setLoading] = useState(true)
-
-
-    useEffect(()=>{
-        setLoading(true)
-        getReviewById(review_id)
-        .then((thisReview)=>{
-            setLoading(false)
-            setReviewNumber(thisReview)
-        })
-    },[review_id]) 
-    
-
-    return loading ?
-            (<p>...Loading</p>):
-        (
-
-            <div className="reviewById-card">
-            <li>
-                <h2>{reviewNumber.title}</h2>
-                <img src={reviewNumber.review_img_url} alt={reviewNumber.title} className="reviewById-card-Img"></img>
-                <p>{reviewNumber.review_body}</p>
-                <h4>{reviewNumber.votes}</h4>
-                <h4>{reviewNumber.owner}</h4>
-                <p>{reviewNumber.category}</p>
-            </li>
-
-        </div>
-    )
-    
-}
+  return loading ? (
+    <p>...Loading</p>
+  ) : (
+    <div className="reviewById-card">
+      <h2>{reviewById.title}</h2>
+      <img
+        src={reviewById.review_img_url}
+        alt={reviewById.title}
+        className="reviewById-card-Img"
+      ></img>
+      <p>{reviewById.review_body}</p>
+      <h4>{reviewById.votes}</h4>
+      <h4>{reviewById.owner}</h4>
+      <p>{reviewById.category}</p>
+    </div>
+  );
+};
