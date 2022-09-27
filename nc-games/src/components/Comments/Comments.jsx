8048fa8335d248cee8e344829ed393056461018e
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-27 12:07:31 +0100

    added comments and a linebreaker for css later

--- nc-games/src/components/Comments/Comments.jsx
+++ nc-games/src/components/Comments/Comments.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { commentsById } from "../../utils/api";
 
 
 export const Comments = () =>{
@@ -8,6 +9,30 @@ export const Comments = () =>{
     const [comments, setComments] = useState([])
 
     useEffect(()=>{
-        //code
+        commentsById(review_id)
+        .then((apiCommentsById) => {
+            setComments(apiCommentsById)
+        })
     },[review_id])
+
+    return (
+        <div className="comments-box">
+            <h2>Comments</h2>
+            <ul>
+                {comments.map((comment)=>{
+                    return(
+                        <>
+                        <li key={comment.comment_id}>
+                            <h4>Made By{comment.author}</h4>
+                            <p>{comment.body}</p>
+                            <p>This has {comment.votes} likes</p>
+                            <p>{comment.created_at}</p>
+                            <p className="lineBreaker"></p>
+                        </li>
+                        </>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
