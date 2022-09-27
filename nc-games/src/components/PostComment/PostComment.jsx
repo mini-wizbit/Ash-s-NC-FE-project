b4d4e094a98ec9369f6b004eef6b5f13c31d3150
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-27 14:03:19 +0100

    prep now to make a request in the utils/api.js

--- nc-games/src/components/PostComment/PostComment.jsx
+++ nc-games/src/components/PostComment/PostComment.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export const PostComment = () =>{
-    //code
+
+    const {review_id} = useParams()
+
+
+    const submit =(e)=>{
+        e.preventDefault();
+    }
+
+    return(
+        <div className="post-a-comment">
+            <p>post stuff here </p>
+            <form onSubmit={submit}>
+            <input type="text" placeholder="Your comment here" required />
+            <button type="submit">turnary here to confrim clicked!</button>
+            </form>
+        </div>
+        
+    )
 }
