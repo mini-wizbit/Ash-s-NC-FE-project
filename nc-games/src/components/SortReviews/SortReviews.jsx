a3c8921307d3f85acd0767d02e13d904790934ba
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-29 10:36:58 +0100

    forrmated

--- nc-games/src/components/SortReviews/SortReviews.jsx
+++ nc-games/src/components/SortReviews/SortReviews.jsx
@@ -5,34 +5,32 @@ import { reviewsByQueries } from "../../utils/api";
 import { OrderByX } from "../OrderByX/OrderByX";
 import { SortByX } from "../SortByX/SortByX";
 
-export const SortReviews =({setReviews, theCategory})=>{
-    //code
-    const{review} = useParams()
-    const[loading, setLoading] = useState(true)
-    const[sortByValue, setSortByValue] = useState(undefined) //<-?
-    const[orderByValue, setOrderByValue]= useState("desc")
-    
-    console.log(review, "<<");
+export const SortReviews = ({ setReviews, theCategory }) => {
+  const { review } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [sortByValue, setSortByValue] = useState(undefined);
+  const [orderByValue, setOrderByValue] = useState("desc");
 
-    useEffect(()=>{
-        setLoading(true)
-        reviewsByQueries(theCategory,sortByValue, orderByValue)
-        .then((reviewsFromApi)=>{
-            setReviews(reviewsFromApi)
-            setLoading(false)
-        })
-    },[sortByValue, orderByValue])
+  useEffect(() => {
+    setLoading(true);
+    reviewsByQueries(theCategory, sortByValue, orderByValue).then(
+      (reviewsFromApi) => {
+        setReviews(reviewsFromApi);
+        setLoading(false);
+      }
+    );
+  }, [sortByValue, orderByValue]);
 
-    console.log(orderByValue, "<<< order");    return(
-        <div>
-            <SortByX 
-            sortByValue={sortByValue}
-            setSortByValue={setSortByValue}
-            ></SortByX>
-            <OrderByX
-            orderByValue={orderByValue}
-            setOrderByValue={setOrderByValue}
-            ></OrderByX>
-        </div>
-    )
-}
+  return (
+    <div>
+      <SortByX
+        sortByValue={sortByValue}
+        setSortByValue={setSortByValue}
+      ></SortByX>
+      <OrderByX
+        orderByValue={orderByValue}
+        setOrderByValue={setOrderByValue}
+      ></OrderByX>
+    </div>
+  );
+};
