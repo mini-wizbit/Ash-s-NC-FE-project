6250ce04590a7a8928724a3d183fbedb033c39dc
mini-wizbit <ash.ingham34@googlemail.com>
2022-09-29 10:37:40 +0100

    formatted

--- nc-games/src/components/SortByX/SortByX.jsx
+++ nc-games/src/components/SortByX/SortByX.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 
-export const SortByX = ({sortByValue, setSortByValue}) =>{
+export const SortByX = ({ sortByValue, setSortByValue }) => {
+  const handleSort = (e) => {
+    setSortByValue(e.target.value);
+  };
 
-    const handleSort =(e)=>{
-        setSortByValue(e.target.value)
-    }
-
-    return (
-        <>
-        <label>
-            Sort By:
-            <select 
-            className="dropdown"
-            onChange={handleSort}
-            value={sortByValue}
-            >
-            <option value='title'>Title</option>
-            <option value="owner">Owner</option>
-            <option value="votes">Votes</option>
-            <option value="created_at">Date</option>
-            </select>
-        </label>
-        </>
-    )
-}
+  return (
+    <>
+      <label>
+        Sort By:
+        <select className="dropdown" onChange={handleSort} value={sortByValue}>
+          <option value="title">Title</option>
+          <option value="owner">Owner</option>
+          <option value="votes">Votes</option>
+          <option value="created_at">Date</option>
+        </select>
+      </label>
+    </>
+  );
+};
